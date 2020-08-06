5d97d9b4679df0c1de6a1076480b33db8cc6df49
s1270232 <69312680+s1270232@users.noreply.github.com>
2020-08-06 23:43:42 +0900

    HoT: Initial  revision

---
+++ ex4.py
@@ -0,0 +1,16 @@
+import random
+heads = 0
+tails = 0
+
+print("Tossing a coin...")
+
+for i in range(3):
+    num = random.randint(0,1)
+    if (num % 2 == 0):
+        print(f"Round {i + 1}: Head")
+        heads += 1
+    else:
+        print(f"Round {i + 1}: Tail")
+        tails += 1
+
+print(f"Heads: {heads}, Tails: {tails}")
